2ff359c59c9a82103e76da073c7483393241fcec
Dennis T Yip <t80532292@hotmail.com>
2015-04-28 19:10:38 -0700

    first attempt

--- server/enhancedDate.js
+++ server/enhancedDate.js
@@ -1,1 +1,76 @@
-// export this as a Node module using the enhancedDate.js code from Module_2
+/**
+ * Created by toq on 28/04/15.
+ */
+var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var monthNames = ['January', 'Feburary', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'];
+
+var myDate = null;
+var dateIsSet = false;
+
+function setDate(date) {
+    if (!dateIsSet) {
+        setDate();
+    }
+    if (date instanceof Date || typeof date === 'number') {
+        myDate = new Date(date);
+    } else {
+        myDate = new Date();
+    }
+}
+
+function getDate(returnObj) {
+    if (!dateIsSet) {
+        setDate();
+    }
+    if (returnObj) {
+        return myDate;
+    } else {
+        return myDate.getTime();
+    }
+}
+
+function getDayName() {
+    if (!dateIsSet) {
+        setDate();
+    }
+    return dayNames[myDate.getDay()];
+}
+
+function getMonthName() {
+    if (!dateIsSet) {
+        setDate();
+    }
+    return monthNames[myDate.getMonth()];
+}
+
+function isToday() {
+    if (!dateIsSet) {
+        setDate();
+    }
+    var today = new Date();
+    if (today.getMonth === myDate.getMont() &&
+        today.getYear() === myDate.getYear() &&
+        today.getDate() === myDate.getDate()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isFuture() {
+    if (!dateIsSet) {
+        setDate();
+    }
+    var now = new Date();
+    return myDate > now;
+}
+
+module.exports = {
+    setDate: setDate,
+    getDate: getDate,
+    getDayName: getDayName,
+    getMonthName: getMonthName(),
+    isToday: isToday,
+    isFuture: isFuture
+}
--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
-// make this a simple http server that writes to the response stream object
+/**
+ * Created by toq on 28/04/15.
+ */
 
-// the page should display the following message:
+var http = require('http');
+var enhancedDate = require('./enhancedDate');
+var filterByDay = require('./trucks');
 
-// Today is <day name>, <month name> <date>. Here are the available food trucks:
+http.createServer(function(request, response) {
+    var currentDay = enhancedDate.getDayName();
+    var currentMonth = enhancedDate.getMonthName();
+    var trucks = filterByDay(currentDay);
+    var truckHTML = '<ul>';
+    var i;
+    var l;
+    truck.forEach(function (truck) {
+       truckHTML += '<li>' + turck.name + '</li>';
+    });
+    truckHTML += '</ul>';
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('<h3>Today is ' + currentDay + ' ' + currentMonth + ' ' + date +
+                    'Here are the available food trucks:</h3>');
+    response.write(truckHTML);
+    response.end();
+}).listen(3000, function() {
+    console.log('listening on port 3000');
+});
 
-// list the food trucks returned by filterByDay, passing in the current day name.
-// e.g. filterByDay(day); where "day" is determined using the enhancedDate module
-// The list of trucks returned will be an array of food truck objects. Iterate 
-// through the list, building up a string of food truck names. Once you're done 
-// iterating through that list, display the string you built up.
-
-// Remember that the response is a stream object that must be closed.
